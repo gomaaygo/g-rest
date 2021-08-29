c55e804ec5a29ea9d5b9b65b1c7a722caff859ed
gomaaygo <maiurygarcia@gmail.com>
2021-08-29 13:49:02 -0300

    Added models inputodproduct and outputofproduct

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 # Imports
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
 
 
 class Category(models.Model):
@@ -28,8 +29,8 @@ class Product(models.Model):
 
 
     class Meta:
-        verbose_name="Produto"
-        verbose_name_plural="Produtos"
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
 
     def __str__(self):
         return self.name
@@ -39,17 +40,71 @@ class Stock(models.Model):
     product = models.ForeignKey(Product, verbose_name=("Produto"), 
                         null=False, blank=False, 
                         on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(verbose_name='Quantidade em Estoque', 
+    quantity = models.PositiveIntegerField(verbose_name="Quantidade em Estoque", 
                         null=False, blank=False, default=0,
                         validators=[MinValueValidator(0)])
-    quantity_min = models.PositiveIntegerField(verbose_name='Quantidade Mínima', 
+    quantity_min = models.PositiveIntegerField(verbose_name="Quantidade Mínima", 
                         null=False, blank=False, default=0, 
                         validators=[MinValueValidator(0)])
 
 
     class Meta:
-        verbose_name="Estoque"
-        verbose_name_plural="Estoques"
+        verbose_name = "Estoque"
+        verbose_name_plural = "Estoques"
 
     def __str__(self):
-        return str(self.quantity)
+        return self.product.name
+
+
+class InputOfProduct(models.Model):
+    product = models.ForeignKey(Product, verbose_name=("Produto"), 
+                    null=False, blank=False, 
+                    on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name="Quantidade", 
+                        null=False, blank=False, default=0,
+                        validators=[MinValueValidator(0)])
+    entry_date = models.DateTimeField(verbose_name="Data de entrada",
+                                      auto_now_add=True)
+    expiration_date = models.DateField("Data de validade", null=True, blank=True)
+
+
+    class Meta:
+        verbose_name = "Entrada de Produto"
+        verbose_name_plural = "Entrada de Produtos"
+
+    def __str__(self):
+        return str(self.product.name)
+
+    def save(self, *args, **kwargs):
+        stock = Stock.objects.get(product=self.product)
+        stock.quantity += self.quantity
+        stock.save()
+        super(InputOfProduct, self).save()
+
+
+class OutputOfProduct(models.Model):
+    product = models.ForeignKey(Product, verbose_name=("Produto"), 
+                    null=False, blank=False, 
+                    on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name="Quantidade", 
+                        null=False, blank=False, default=0,
+                        validators=[MinValueValidator(0)])
+    departure_date = models.DateTimeField(verbose_name="Data de saída",
+                                      auto_now_add=True)
+
+
+    class Meta:
+        verbose_name = "Saída de Produto"
+        verbose_name_plural = "Saída de Produtos"
+
+    def __str__(self):
+        return str(self.product.name)
+
+    def save(self, *args, **kwargs):
+        stock = Stock.objects.get(product=self.product)
+        if self.quantity <= stock.quantity:
+            stock.quantity = stock.quantity - self.quantity
+            stock.save()
+        else:
+            raise ValidationError("Não possui quantidade suficiente desse produto!")
+        super(OutputOfProduct, self).save()
