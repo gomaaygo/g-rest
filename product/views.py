74d2e90b8a7b7e5295f03f2712e9ef0955acdc7c
Maiury S. Garcia <maiurygarcia@gmail.com>
2021-12-13 12:19:43 -0300

    Added view edit product

--- product/views.py
+++ product/views.py
@@ -1,10 +1,11 @@
 # Imports
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView
 from django.contrib import messages
 from account.permissions import GroupRequiredMixin
+from django.views.generic import UpdateView
 
 from .models import Product, Stock, InputOfProduct, OutputOfProduct, Category
 from .forms import ProductForm, InputOfProductForm, OutputOfProductForm, CategoryForm
@@ -110,3 +111,14 @@ class OutputOfProductListView(GroupRequiredMixin, ListView):
     group_required = [u'Gerente', u'Caixa']
     model = OutputOfProduct
     queryset = OutputOfProduct.objects.all().order_by("-departure_date")
+
+
+class EditProductView(GroupRequiredMixin, UpdateView):
+    group_required = [u'Gerente', u'Caixa']
+    model = Product
+    template_name = 'product/edit_product.html'
+    form_class = ProductForm
+
+    def get_success_url(self):
+        messages.success(self.request, "Produto atualizado com sucesso!")  
+        return reverse('product:list')
