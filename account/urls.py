c61fc777deff5be7e7b3575bc49a4aa3cca26003
Maiury S. Garcia <maiurygarcia@gmail.com>
2022-04-11 10:56:04 -0300

    Account list added

--- account/urls.py
+++ account/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('register/', views.AccountCreateView.as_view(), name='register'),
     path('<int:pk>/edit/', views.EditAccountView.as_view(), name='edit'),
     path('newpass/', views.NewPasswordView.as_view(), name='new-password'),
-    path('<int:pk>/profile/', views.AccountDetailView.as_view(), name='account-detail'),  
+    path('<int:pk>/profile/', views.AccountDetailView.as_view(), name='account-detail'),
+    path('list/', views.AccountListView.as_view(), name="list"), 
 ]
--- account/views.py
+++ account/views.py
@@ -7,11 +7,12 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
-from django.views.generic import UpdateView, TemplateView
+from django.views.generic import UpdateView, TemplateView, ListView
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import Group
 from django.views.generic.detail import DetailView
 from django.views.decorators.csrf import csrf_exempt
+from account.permissions import GroupRequiredMixin
 
 from .models import Account
 from .forms import AccountForm
@@ -58,6 +59,7 @@ class AccountCreateView(CreateView):
             group = get_object_or_404(Group, name="Gerente")
 
         self.object.password = make_password(self.request.POST["password"])
+        self.object.is_active = False
         self.object.groups.add(group)
         self.object.save()
 
@@ -99,3 +101,8 @@ class AccountDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['user'] = self.get_account()
         return context
+
+
+class AccountListView(GroupRequiredMixin, ListView):
+    group_required = [u'Gerente'] 
+    model = Account
