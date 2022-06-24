e690210cc07b112ffab01c3a9fb56c5c5640f344
Maiury Santos Garcia <maiurygarcia@nre.tec.br>
2022-06-23 22:35:58 -0300

    Updated urls product

--- product/urls.py
+++ product/urls.py
@@ -14,8 +14,10 @@ urlpatterns = [
     path('output/stock/', views.OutputOfProductStockView.as_view(), name="output-product-stock"),
     path('stock/list/', views.StockListView.as_view(), name="stock-list"),
     path('category/add/', views.CategoryAddView.as_view(), name="category-add"),
-    path('input/stock/list/', views.InputOfProductSaleListView.as_view(), name="input-of-product-list"),
-    path('output/stock/list/', views.OutputOfProductSaleListView.as_view(), name="output-of-product-list"),
+    path('input/stock/list/', views.InputOfProductSaleListView.as_view(), name="input-of-product-sale-list"),
+    path('output/stock/list/', views.OutputOfProductSaleListView.as_view(), name="output-of-product-sale-list"),
+    path('input/deposit/list/', views.InputOfProductConsumptionListView.as_view(), name="input-of-product-consumption-list"),
+    path('output/deposit/list/', views.OutputOfProductConsumptionListView.as_view(), name="output-of-product-consumption-list"),
     path('category/list/', views.CategoryListView.as_view(), name="category-list"),
     path('category/<int:pk>/edit/', views.EditCategoryView.as_view(), name='category-edit'),
 ]
